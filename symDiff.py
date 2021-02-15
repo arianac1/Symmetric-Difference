f766adc72376817c5b4a75292b8f3c128bf26cfd
Ariana Casarrubias <33636646+arianac1@users.noreply.github.com>
2021-02-15 11:38:21 -0500

    Create symDiff.py

---
+++ symDiff.py
@@ -0,0 +1,30 @@
+#input
+M=int(raw_input())
+m=raw_input()
+N=int(raw_input())
+n=raw_input()
+
+#splitting and mapping(string to int_list)
+x=list(map(int,m.split()))
+y=list(map(int,n.split()))
+
+#creation of sets
+a=set(x)
+b=set(y)
+
+#difference in each sets
+c=a.difference(b)
+d=b.difference(a)
+
+#union of difference
+e=c.union(d)
+
+#converting set to a list
+result=list(e)
+
+#sorting
+result.sort()
+
+#iteration
+for i in range(len(result)):
+    print(result[i])
